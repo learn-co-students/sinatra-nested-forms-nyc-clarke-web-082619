10f176d7271903879c9435ed7793b42a9e709588
github username <github email address>
2019-09-17 20:45:26 -0400

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,25 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do 
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirates = Pirates.new(params["pirate"]["name"], params["pirate"]["weight"], params["pirate"]["height"])
+
+      params["pirate"]["ships"].each do |ship|
+        Ship.new(ship["name"], ship["type"], ship["booty"])
+      end
+      
+      @ships = Ship.all
+      
+      erb :show
+    end 
+    
   end
 end
